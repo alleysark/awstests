a73efbf1efde7333fbdf93e15109bc60bc6c018d
alleysark <dev.alleysark@gmail.com>
2023-03-14 14:42:12 +0900

    apply serverless.ts

---
+++ hello-api-ts/serverless.ts
@@ -0,0 +1,30 @@
+import type { AWS } from "@serverless/typescript";
+
+const config: AWS = {
+  service: "hello-api-ts",
+  frameworkVersion: "3",
+  provider: {
+    name: "aws",
+    runtime: "nodejs18.x",
+    stage: "dev",
+    region: "ap-northeast-2"
+  },
+  functions: {
+    hello: {
+      handler: "handler.hello",
+      events: [
+        {
+          httpApi: {
+            path: "/hello",
+            method: "get"
+          }
+        }
+      ]
+    }
+  },
+  plugins: [
+    "serverless-webpack"
+  ]
+};
+
+export = config;
