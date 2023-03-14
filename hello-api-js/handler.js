948438c7a373ca6f2f195a90e3448ce0ef19b18a
alleysark <dev.alleysark@gmail.com>
2023-03-14 12:24:15 +0900

    get query param 'name'

--- hello-api-js/handler.js
+++ hello-api-js/handler.js
@@ -1,18 +1,14 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  if (!event.queryStringParameters || !event.queryStringParameters.name) {
+    return {
+      statusCode: 404,
+      body: 'Not Found'
+    };
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: `Hello, ${event.queryStringParameters.name}!`,
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
